0b6ce7dd118837a5f549b57e44ff1232e160f073
Chloe Rodriguez <chloerod2002@hotmail.com>
2023-03-23 21:50:40 -0400

    Tip calculator express application created

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ app.get('/greeting/:name', (rq, res) =>{
     res.send("Hello" + name)
 })
 
+//tip calc
+app.get("/tip/:total/:percentage", (req, res) =>{
+    const total = req.params.total
+    const tipPercentage =  req.params.percentage/100
+    const tipAmount = total * tipPercent
+    res.send("your tip amount is $" + tipAmount)
+})
